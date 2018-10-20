c0f00d29abc086242780948eda164cad53f8b6f4
Leipeng.Niu <Leipeng.Niu@moodys.com>
2018-10-20 15:33:42 +0800

    save all urls to text

--- index.py
+++ index.py
@@ -8,13 +8,40 @@ def getHtmlText(url):
         r = requests.get(url)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
-        text = r.content
-        soup = BeautifulSoup(text, "html.parser")
-        a = soup.find_all('a')
-        return a
+        return r.text
     except:
         return "Error occurs."
 
-a = getHtmlText(baseUrl)
 
-print(a)
+def getContent(url):
+    contents = []
+    html = getHtmlText(url)
+
+    soup = BeautifulSoup(html, "lxml")
+    booklistDom = soup.find("div", class_="booklist")
+    booklistContents = booklistDom.contents
+    for span in booklistContents:
+        content = {}
+        linkDom = span.find("a")
+        if (linkDom == None):
+            continue
+        content["title"] = linkDom.text
+        content["url"] = linkDom["href"]
+        contents.append(content)
+    return contents
+
+
+def save2File(contents):
+    with open("./data/mianzhuan.txt", "a+", encoding="utf-8") as f:
+        for content in contents:
+            f.write("{} \t {} \t \n".format(
+                content["title"], content["url"]))
+
+
+def main(baseUrl):
+    contents = getContent(baseUrl)
+    save2File(contents)
+
+
+if (__name__ == "__main__"):
+    main(baseUrl)
